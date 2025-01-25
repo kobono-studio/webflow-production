29b6a8f41610dfb9b50a190b27691f3437f969a5
Pierre Lovenfosse <pierre.lovenfosse@badsmuggler.com>
2025-01-25 21:37:30 +0100

    library - update kobono object

--- library/slater/kobono.js
+++ library/slater/kobono.js
@@ -2,12 +2,6 @@ window.Kobono = window.Kobono || {
   breakpoints: {},
   factories: {},
   lenis: null,
-  loaded: {
-    breakpoints: false,
-    gsap: false,
-    lenis: false,
-    useSwiper: false,
-    useYoutube: false,
-  },
+  loaded: {},
   ready: false,
 }
